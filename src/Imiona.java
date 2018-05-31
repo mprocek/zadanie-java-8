fa6e588aabd057ffc550ce4762c4872a7d23a6c0
Marek Procek <marekprocek@gmail.com>
2018-05-31 13:15:21 +0200

    zadanie-java-8 całość

---
+++ src/Imiona.java
@@ -0,0 +1,9 @@
+public class Imiona {
+    public static void main(String[] args){
+        String[] imiona = {"Basia","Marek","Albert"};
+
+        for(int i=0; i<imiona.length; i++){
+            System.out.println(imiona[i]);
+        }
+    }
+}
